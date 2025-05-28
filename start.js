119415bb9af5e31e17853a523370304afc816d8e
kojo akomiah <kojoakomiah1@gmail.com>
2025-05-28 05:26:03 +0000

    Add files via upload

---
+++ start.js
@@ -0,0 +1,12 @@
+ const massMark= 78;
+   const heightMark=1.95;
+   const massJohn=65;
+   const heightJohn=1.86;
+
+const BMIMark=massMark/heightMark**2;
+const BMIJohn=massJohn/heightJohn**2;
+
+   
+
+   const markHigherBMI=BMIJohn>BMIMark
+   console.log(BMIJohn,BMIMark,markHigherBMI);
