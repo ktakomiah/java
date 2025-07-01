79c4fd167fb406be91ea9be66f94fb3b5c3a9b87
kojo akomiah <kojoakomiah1@gmail.com>
2025-07-01 09:27:26 +0000

    Add files via upload

---
+++ first day.py
@@ -0,0 +1,16 @@
+print("hello world!")
+print("Day 1- python print Function")
+print("The function is declared like this")
+
+print("print('what to print')")
+print("hello world! \nhello world!\nhello world!\nhello world!\nhello world!")
+
+# concantenation
+print("hello"+" "+"Angelo")
+# input function usage
+print("hello " +input("what is your name? "))
+
+# challenge
+
+
+print(len(input("what is your name?      ")))
